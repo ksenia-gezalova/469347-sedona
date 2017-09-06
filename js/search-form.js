8d230a37c440275d1a65e9581fdbe2923cd36703
ksenia-gezalova <ksenia.gezalova@gmail.com>
2017-09-06 21:19:48 +0300

    add js and iframe
    add js for  search-form, add iframe and bg map if it wouldn't work

---
+++ js/search-form.js
@@ -0,0 +1,8 @@
+var btn = document.querySelector(".btn-search-appartments");
+var searchForm = document.querySelector(".search-form");
+var dateCheckIn = searchForm.querySelector("#check-in-date");
+var dateCheckOut = searchForm.querySelector("#check-out-date");
+var form = searchForm.querySelector("form");
+var adults = searchForm.querySelector("#adults");
+var children = searchForm.querySelector("#children");
+
