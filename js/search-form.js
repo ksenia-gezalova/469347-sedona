a57dec214a85ffbfc727daade16172e2e3782cde
ksenia-gezalova <ksenia.gezalova@gmail.com>
2017-09-07 20:44:50 +0300

    Add final details
    css prefixers
    minificate css and js

--- js/search-form.js
+++ js/search-form.js
@@ -1,8 +1,28 @@
 var btn = document.querySelector(".btn-search-appartments");
 var searchForm = document.querySelector(".search-form");
-var dateCheckIn = searchForm.querySelector("#check-in-date");
-var dateCheckOut = searchForm.querySelector("#check-out-date");
-var form = searchForm.querySelector("form");
-var adults = searchForm.querySelector("#adults");
-var children = searchForm.querySelector("#children");
+var dateCheckIn = searchForm.querySelector("[name=check-in-date]");
+var dateCheckOut = searchForm.querySelector("[name=check-out-date]");
+var adults = searchForm.querySelector("[name=adults]");
+var children = searchForm.querySelector("[name=children]");
 
+btn.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    searchForm.classList.toggle("hide-search");
+    dateCheckIn.focus();
+});
+
+searchForm.addEventListener("submit", function (evt) {
+    if (!dateCheckIn.value || !dateCheckOut.value || !adults.value) {
+        evt.preventDefault();
+        console.log("Нужно указать дату заезда и дату выезда, а также количество гостей");
+    } else {
+        localStorage.setItem("adults", adults.value);
+        localStorage.setItem("children", children.value);
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        searchForm.classList.add("hide-search");
+    }
+});
